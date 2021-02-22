e14fc427c0e3e99819dbf619f92f51c84e57a3c3
Jussimar Nascimento Leal <jussimar@jussimarleal.com.br>
2021-02-22 01:31:29 +0000

    congiração da framework e criação da nova pagina incial e sua controller

--- public/application/config/config.php
+++ public/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://8001-harlequin-damselfly-a970kx2g.ws-us03.gitpod.io/';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'dpYCyTAUytqLbY6zYqs2OvYVRzc2qpni';
 
 /*
 |--------------------------------------------------------------------------
