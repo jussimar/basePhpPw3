60dd7943e69b31aedc2ee28467fee1f62e8425a8
Jussimar Nascimento Leal <jussimar@jussimarleal.com.br>
2021-03-07 14:55:01 +0000

    criação da tela principal

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -7,8 +7,111 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home - Site PW3</title>
+	<link rel="stylesheet" href="<?= base_url() ?>assets/reset.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/principal.css">
 </head>
 <body>
-	<h1>Seja bem vindo</h1>
+	<div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron cont">
+					<h1 class="display-4">Seja bem vindo!</h1>
+					<p class="lead">Vamos Aprender a criar um site php usando framework</p>
+					
+				</div>
+            </div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Pagina 1</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Pagina 2</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row cont">
+			<div class="col-6">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+			<div class="col-6">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Titulo</h3>
+				<img src="<?= base_url() ?>/assets/imagens/logo-codeigniter.jpg" alt="" class="img-fluid">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel dolor 
+					neque. Sed ut sem nisi. Etiam congue sem eget tortor dictum laoreet. 
+					Duis blandit molestie porttitor. Aenean a ullamcorper felis. Nam leo nisl, 
+					iaculis id eros vitae, tempor commodo est. Morbi at dolor tincidunt, 
+					eleifend purus nec, faucibus ligula.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 rodape">
+				<p>Tema criado na aula de programação web 3 - 2021</p>
+			</div>
+		</div>
+    </div>
 </body>
 </html>
